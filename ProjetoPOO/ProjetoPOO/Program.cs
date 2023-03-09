1b6bfa6a5c394551172645eb42c50fa345b27b2c
2022103030018 <Mateusorocondo78@gmail.com>
2023-03-09 14:21:20 -0400

    Criação de Projeto

---
+++ ProjetoPOO/ProjetoPOO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bab");
+            Console.ReadKey();
+        }
+    }
+}

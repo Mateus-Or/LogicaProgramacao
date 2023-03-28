574081173bc3f4aa8f74157fad143f4bf9cb6a46
2022103030018 <Mateusorocondo78@gmail.com>
2023-03-28 10:58:21 -0400

    28/03

---
+++ QuestoesDeIf_Else/Questão09/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Calcula Aumento Salarial---");
+            Console.WriteLine("\nCargos: \n\t[1] Gerente \n\t[2] Engenheiro \n\t[3] Técnico");
+            Console.Write("Informe o seu cargo:");
+            int cargo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe seu salario: ");
+            double salario = Convert.ToDouble(Console.ReadLine());
+
+            double novoSalar = 0;
+            switch (cargo)
+            {
+                case 1:
+                    novoSalar = salario * 1.1;
+                    break;
+                case 2:
+                    novoSalar = (salario * 1.2);
+                    break;
+                case 3:
+                    novoSalar = salario * 1.3;
+                    break;
+                default:
+                    novoSalar = salario * 1.4;
+                    break;
+            }
+
+            Console.WriteLine($"O salario era de {salario:c2}, mas passara a ser {novoSalar:c2}" +
+                $"\nA diferença é de {novoSalar-salario:c2}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuestoesDeIf_Else/Questão10/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questão10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int media = 0;
+            char conceito = ' ';
+            string situacao;
+
+            Console.Write("Favor digite tu nombre: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a nota do 1° Bimentre: ");
+            int nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a nota do 2° Bimentre: ");
+            int nota2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a nota do 3° Bimentre: ");
+            int nota3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a nota do 4° Bimentre: ");
+            int nota4 = Convert.ToInt32(Console.ReadLine());
+
+            media = (int)(nota1 + nota2 + nota3 + nota4)/4;
+
+            if (media >= 90)
+            {
+                conceito = 'A';
+            }
+            else if (media >= 80)
+            {
+                conceito = 'B';
+            }
+            else if (media >= 60)
+            {
+                conceito = 'C';
+            }
+            else if (media >= 40)
+            {
+                conceito = 'D';
+            }
+            else if (media >= 20)
+            {
+                conceito = 'E';
+            }
+
+
+            switch (conceito)
+            {
+                case 'A':
+                    situacao = "Aprovado";
+                    break;
+                case 'B':
+                    situacao = "Aprovado";
+                    break;
+                case 'C':
+                    situacao = "Aprovado";
+                    break;
+                case 'D':
+                    situacao = "Recuperação";
+                    break;
+                case 'E':
+                    situacao = "Exame";
+                    break;
+                default:
+                    situacao = "Reprovado";
+                    break;
+            }
+
+            Console.WriteLine($"\t\t---Informações---\n" +
+                $"Nome: {nome}\n" +
+                $"Conseito: {conceito}\n" +
+                $"Situação: {situacao}\n");
+
+            Console.ReadKey();
+        }
+    }
+}

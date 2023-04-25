d67687cfd8b7ebfced517ed8adb389f9be9c6286
2022103030018 <Mateusorocondo78@gmail.com>
2023-04-25 11:15:42 -0400

    25/04

---
+++ repetição/ajudaConta/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ajudaConta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\t---Ajuda nas Compras---");
+
+            Console.Write("Informe o valor disponivel: ");
+            double saldo = double.Parse(Console.ReadLine());
+
+            double compra = 0;
+            double somaCompra = 0;
+            int cont = 0;
+            bool podeComprar = true;
+
+            do
+	        {
+                Console.Write("Valor da compra: ");
+                compra = double.Parse(Console.ReadLine());
+
+                if (compra > 0)
+	            {
+                    if (compra <= saldo)
+	                {
+                        saldo = saldo - compra;
+                        somaCompra += compra;
+                        cont++;
+                        Console.WriteLine($"\nCompra Registrada!" +
+                            $"\n\nValor restante do saldo {saldo:c2}");
+
+	                }
+                    else
+                    {
+                        podeComprar = false;
+                        Console.WriteLine("Valor do saldo insuficiente!");
+                    }
+	            }
+                else
+                {
+                    Console.WriteLine("Saldo insuficiente!");
+                    Console.ReadLine();
+                }
+
+	        } while (podeComprar);
+            Console.Clear();
+            Console.WriteLine("\t\t---Resultado das compras");
+            Console.WriteLine($"Numero de compras {cont}" +
+                $"\nTotal gasto {somaCompra:c2}" +
+                $"\nSaldo restante {saldo:c2}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ repetição/numeroVotos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numeroVotos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
